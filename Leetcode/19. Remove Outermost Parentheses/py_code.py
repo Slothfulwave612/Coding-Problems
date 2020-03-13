4adb72423b3c8c7b0ddc0df8ad8615667604fe66
Anmol Durgapal <slothfulwave10@gmail.com>
2020-03-13 22:48:04 +0530

    Create py_code.py

---
+++ Leetcode/19. Remove Outermost Parentheses/py_code.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeOuterParentheses(self, S):
+        a = [1 if item == '(' else -1 for item in S ]
+
+        acc_sum = 0
+        count = 0
+        b = 0
+        l = []
+
+        for item in a:
+            acc_sum += item
+            a[count] = acc_sum
+            count += 1
+
+        for i in range(len(a)):
+            if a[i] == 0:
+                l.append(S[b:i+1])
+                b = i + 1
+        
+        return ''.join(grp[1:-1] for grp in l)
